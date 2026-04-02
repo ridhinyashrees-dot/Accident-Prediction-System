dc279dce313f2fae7ba95776b968594b1ffcf256
ridhinyashrees2025cse-sys <ridhinyashree.s2025cse@sece.ac.in>
2026-04-02 15:53:11 +0530

    Cleaned migration files and app config

--- accident_dashboard/settings.py
+++ accident_dashboard/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
-from django_mongodb_backend.fields import ObjectIdAutoField
+from django_mongodb_backend.fields import ObjectIdField
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,4 +127,4 @@ LOGIN_REDIRECT_URL = '/home/'
 ALLOWED_HOSTS = ['*']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-DEFAULT_AUTO_FIELD = 'django_mongodb_backend.fields.ObjectIdAutoField'
+DEFAULT_AUTO_FIELD = 'django_mongodb_backend.fields.ObjectIdField'
